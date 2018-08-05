6f2e738b05eed71d8a3a394520c93eecfc956062
sharoue <35909037+sharoue@users.noreply.github.com>
2018-08-05 18:14:01 +0800

    更新证书提取工具

--- 一键屏蔽软件/证书提取工具/~GetCertFromFolder.ps1
+++ 一键屏蔽软件/证书提取工具/~GetCertFromFolder.ps1
@@ -1,10 +1,15 @@
-# Coded by rewqazxv (https://github.com/rewqazxv)
 $exportType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
 foreach ($exeName in (ls *.exe).BaseName) {
     $exeFile = $exeName + '.exe'
     $cert = (Get-AuthenticodeSignature $exeFile).SignerCertificate
+	$hashstr = $cert.GetCertHashString()
+	$hashalgo = $cert.SignatureAlgorithm.FriendlyName
+	$issueto = $cert.subject.Substring($cert.subject.indexof("CN=")+3)
+	$issueto = $issueto.Substring(0,$issueTo.IndexOf('='))
+	$issueto = $issueto.Substring(0,$issueTo.LastIndexOf(','))
+	$validto = $cert.GetExpirationDateString().split()[0].Replace('/','')
     if ($cert -ne $null) {
-        $outputPath = (pwd).Path + '\' + $cert.GetCertHashString() + '-' + $cert.SignatureAlgorithm.FriendlyName + '-' + $cert.GetExpirationDateString().split()[0].Replace('/','') + '.cer'
+        $outputPath = (pwd).Path + '\' + $hashstr + ' - ' + $hashalgo + ' - ' + $issueto + ' - ' + $validto + '.cer'
         [System.IO.File]::WriteAllBytes($outputPath, $cert.Export($exportType))
     }
 }
