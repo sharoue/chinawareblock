af7169a13ceea467d19ef07418cb99212157fb9f
ChenShihuan <cnchensh@outlook.com>
2020-04-09 19:17:46 +0800

    文件后缀多了一个t

---
+++ 一键屏蔽软件/真的全拉黑.bat
@@ -0,0 +1,50 @@
+@echo off
+:: BatchGotAdmin  
+:-------------------------------------  
+REM  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
+  
+REM --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (  
+    echo Requesting administrative privileges...  
+    goto UACPrompt  
+) else ( goto gotAdmin )  
+  
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
+  
+    "%temp%\getadmin.vbs"  
+    exit /B  
+  
+:gotAdmin  
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
+    pushd "%CD%"  
+    CD /D "%~dp0"  
+:--------------------------------------  
+
+@echo on
+cd .\全家桶及浏览器
+call ~一键拉黑.bat
+cd ..
+cd .\过期证书
+call ~一键拉黑.bat
+cd ..
+cd .\不受欢迎
+call ~一键拉黑.bat
+cd ..
+cd .\不好用
+call ~一键拉黑.bat
+cd ..
+cd .\工作无关软件
+call ~一键拉黑.bat
+cd ..
+cd .\证券炒股
+call ~一键拉黑.bat
+cd ..
+cd .\腾讯
+call ~一键拉黑.bat
+cd ..
+echo All Done!!!
+pause
+
